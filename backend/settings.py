955e2b9fd9a50ae6114495ee68b38b08f0e5b0c2
farhan <amanfarhan1234510@gmail.com>
2026-06-01 14:57:23 +0530

    Temporarily enable debug for deployment fix

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#(nu*0bm_s!94)e1-k$@zj2gk=+i88)(324u+yn=+p!i1pl=g%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.onrender.com',
